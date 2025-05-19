022df69417d707d316c52f66b762c4dcd7f7f3d0
NEEDYOVERDOS3 <novoveteranolegal@hotmail.com>
2025-05-19 08:25:43 -0300

    adicionado mais telas

---
+++ entrada/Perfil/usuario/assets/js/main.js
@@ -0,0 +1,62 @@
+/*=============== EMAIL JS ===============*/
+
+/*================ PLANO DE FUNDO PERFIL ====================*/
+let text = document.getElementById('titulo');
+let evelynnfrente = document.getElementById('eveFrente');
+let evelynnfundo = document.getElementById('eveFundo');
+let btn = document.getElementById('btn');
+
+window.addEventListener('scroll', () =>{
+    let value = window.scrollY;
+
+    text.style.marginTop = value * 0.2 + 'px';
+    text.style.transform = 'scale('+ (1 + value * 0.0003) + ')';
+    btn.style.marginTop = value * 0.3 + 'px';
+    evelynnfrente.style.transform = 'scale('+ (1 + value * 0.0001) + ')';
+    evelynnfrente.style.top = value * -0.03 + 'px';
+})
+/*================ BARRA DE CONTAGEM ====================*/
+const statsData = {
+    combat: {
+        score: 79,
+        damagePerMin: 903,
+        killParticipation: 41,
+        ccScorePerMin: 11.0
+    },
+    farming: {
+        score: 71,
+        goldSpentPerMin: 464,
+        csPerMin: 6.5,
+        xpPerMin: 521
+    },
+    vision: {
+        score: 55,
+        visionScorePerMin: 0.5,
+        wardsPlaced: 8,
+        wardsDestroyed: 2
+    }
+};
+
+// Função para atualizar os valores dos elementos HTML
+function updateStats() {
+    // Combate
+    document.getElementById('combat-score').innerText = statsData.combat.score;
+    document.getElementById('combat-damage').style.width = `${statsData.combat.damagePerMin / 11}%`;
+    document.getElementById('combat-kill').style.width = `${statsData.combat.killParticipation/ 0.99}%`;
+    document.getElementById('combat-cc').style.width = `${statsData.combat.ccScorePerMin * 10}%`;
+
+    // Farming
+    document.getElementById('farming-score').innerText = statsData.farming.score;
+    document.getElementById('farming-gold').style.width = `${statsData.farming.goldSpentPerMin / 3}%`;
+    document.getElementById('farming-cs').style.width = `${statsData.farming.csPerMin * 6}%`;
+    document.getElementById('farming-xp').style.width = `${statsData.farming.xpPerMin / 5}%`;
+
+    // Visão
+    document.getElementById('vision-score').innerText = statsData.vision.score;
+    document.getElementById('vision-score-per-minute').style.width = `${statsData.vision.visionScorePerMin * 60}%`;
+    document.getElementById('vision-wards').style.width = `${statsData.vision.wardsPlaced / 0.12}%`;
+    document.getElementById('vision-wards-destroyed').style.width = `${statsData.vision.wardsDestroyed * 37}%`;
+}
+
+// Chama a função ao carregar a página
+updateStats();
